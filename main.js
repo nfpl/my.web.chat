d76274f7aea816a4f45d2519710e1c4e74d64bde
nfpl <nunofpl@hotmail.com>
2025-04-22 23:43:02 +0100

    “Remove arquivo(s) desnecessário(s)”

--- main.js
+++
@@ -1,9 +0,0 @@
-const { app, BrowserWindow } = require('electron');
-
-let mainWindow;
-
-app.on('ready', () => {
-    mainWindow = new BrowserWindow({ width: 800, height: 600 });
-    mainWindow.loadURL('https://seu-site.com'); // Altere para a URL da sua webpage
-	mainWindow.loadFile('index.html');
-});
